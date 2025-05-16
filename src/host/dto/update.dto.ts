ce0f8636c204aee6d0d86da9be057891ccea8faf
Alec Mather <aymather@gmail.com>
2025-05-15 23:26:36 -0700

    Fixed update host dto

--- src/host/dto/update.dto.ts
+++ src/host/dto/update.dto.ts
@@ -11,8 +11,35 @@ export class UpdateHostBodyDto {
 	@IsString({ allowUndefined: true })
 	name: string;
 
-	@IsString({ allowUndefined: true })
+	@IsString({ allowUndefined: true, nullable: true })
 	profile_image_url: string;
+
+	@IsString({ allowUndefined: true, nullable: true })
+	stage_image_url: string;
+
+	@IsString({ allowUndefined: true, nullable: true })
+	description: string;
+
+	@IsString({ allowUndefined: true, nullable: true })
+	website_url: string;
+
+	@IsString({ allowUndefined: true, nullable: true })
+	instagram_url: string;
+
+	@IsString({ allowUndefined: true, nullable: true })
+	twitter_url: string;
+
+	@IsString({ allowUndefined: true, nullable: true })
+	facebook_url: string;
+
+	@IsString({ allowUndefined: true, nullable: true })
+	youtube_url: string;
+
+	@IsString({ allowUndefined: true, nullable: true })
+	tiktok_url: string;
+
+	@IsString({ allowUndefined: true, nullable: true })
+	x_url: string;
 }
 
 export class UpdateHostResponseDto extends PublicHost {}
